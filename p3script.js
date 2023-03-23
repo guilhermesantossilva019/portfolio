e2de8e917382b890a7b2364ceea77e0548895d4f
Guilherme Santos Silva <127796300+guilhermesantossilva019@users.noreply.github.com>
2023-03-23 18:17:00 -0300

    Update p3script.js

--- p3script.js
+++ p3script.js
@@ -27,7 +27,7 @@ const loop = setInterval(() => {
         mario.style.animation = "none";
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src="images/game-over.png";
+        mario.src="https://guilhermesantossilva019.github.io/portfolio/p3game-over.png";
         mario.style.width="75px"
         mario.style.marginLeft ="50px"
 
